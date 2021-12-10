a0f8c6d4ec42ace7d6aca729ef592fd69aef3ce9
Norman Menes <nmenes@office.cdv.pl>
2021-12-10 19:58:07 +0100

    Dodaj pliki projektów.

---
+++ 2021_ProgramLab__1sem_ProjektGrupowy_Menes.cpp
@@ -0,0 +1,14 @@
+﻿// komentarzami zostaną oznaczone osoby, które odpowiadają za dany element kodu
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
